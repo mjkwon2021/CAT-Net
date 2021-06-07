e1cd7c0d9d1db29dcf5dcd74449c2745cd90be20
CauchyComplete <corundum240@gmail.com>
2021-06-07 15:04:39 +0900

    Tidy networks

--- lib/models/network_CAT.py
+++ lib/models/network_CAT.py
@@ -65,49 +65,6 @@ class BasicBlock(nn.Module):
 
         return out
 
-class HalfDilatedBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(HalfDilatedBlock, self).__init__()
-        conv_planes = planes // 4
-        dilconv_planes = (planes // 4) * 3
-        assert planes == conv_planes + dilconv_planes
-        self.conv1 = nn.Conv2d(inplanes, conv_planes, kernel_size=3, stride=stride, padding=1, bias=False, dilation=1)
-        self.bn1 = nn.BatchNorm2d(conv_planes, momentum=BN_MOMENTUM)
-        self.dil_conv1 = nn.Conv2d(inplanes, dilconv_planes, kernel_size=3, stride=stride, padding=8, bias=False, dilation=8)
-        self.dil_bn1 = nn.BatchNorm2d(dilconv_planes, momentum=BN_MOMENTUM)
-        self.conv2 = nn.Conv2d(inplanes, conv_planes, kernel_size=3, stride=stride, padding=1, bias=False, dilation=1)
-        self.bn2 = nn.BatchNorm2d(conv_planes, momentum=BN_MOMENTUM)
-        self.dil_conv2 = nn.Conv2d(inplanes, dilconv_planes, kernel_size=3, stride=stride, padding=8, bias=False, dilation=8)
-        self.dil_bn2 = nn.BatchNorm2d(dilconv_planes, momentum=BN_MOMENTUM)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out1 = self.conv1(x)
-        out1 = self.bn1(out1)
-        out2 = self.dil_conv1(x)
-        out2 = self.dil_bn1(out2)
-        out = torch.cat([out1, out2], dim=1)
-        out = self.relu(out)
-
-        out1 = self.conv2(out)
-        out1 = self.bn2(out1)
-        out2 = self.dil_conv2(out)
-        out2 = self.dil_bn2(out2)
-        out = torch.cat([out1, out2], dim=1)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
 
 class Bottleneck(nn.Module):
     expansion = 4
@@ -148,69 +105,7 @@ class Bottleneck(nn.Module):
         out = self.relu(out)
 
         return out
-#
-# class dct2d_Conv_Layer(nn.Module):
-#     # This class is written by IJ Yu
-#     def __init__(self, scale, start, num_filters):
-#         super(dct2d_Conv_Layer, self).__init__()
-#         self.scale = scale
-#         self.start = start
-#         self.num_filters = num_filters // 3
-#         self.dct_base = self.load_DCT_basis()
-#         self.conv1, self.conv2, self.conv3 = self.dct2d_Conv(), self.dct2d_Conv(), self.dct2d_Conv()
-#
-#         for conv in [self.conv1, self.conv2, self.conv3]:
-#             conv.weight = nn.Parameter(torch.from_numpy(self.dct_base).float())
-#             conv.weight.requires_grad = False
-#         self.swap = []
-#         for i in range(self.num_filters):
-#             self.swap += [i, i + self.num_filters, i + self.num_filters * 2]
-#
-#     def cal_scale(self, p, q):
-#         if p == 0:
-#             ap = 1 / (np.sqrt(self.scale))
-#         else:
-#             ap = np.sqrt(2 / self.scale)
-#         if q == 0:
-#             aq = 1 / (np.sqrt(self.scale))
-#         else:
-#             aq = np.sqrt(2 / self.scale)
-#
-#         return ap, aq
-#
-#     def cal_basis(self, p, q):
-#         basis = np.zeros((self.scale, self.scale))
-#         ap, aq = self.cal_scale(p, q)
-#         for m in range(0, self.scale):
-#             for n in range(0, self.scale):
-#                 basis[m, n] = ap * aq * np.cos(np.pi * (2 * m + 1) * p / (2 * self.scale)) * np.cos(
-#                     np.pi * (2 * n + 1) * q / (2 * self.scale))
-#         return basis
-#
-#     def load_DCT_basis(self):
-#         basis_64 = np.zeros((self.num_filters, self.scale, self.scale))
-#         idx = 0
-#         for i in range(self.scale * 2 - 1):
-#             cur = max(0, i - self.scale + 1)
-#             for j in range(cur, i - cur + 1):
-#                 if idx >= self.num_filters + self.start:
-#                     return basis_64.reshape((self.num_filters, 1, self.scale, self.scale))
-#                 if idx >= self.start:
-#                     basis_64[idx - self.start, :, :] = self.cal_basis(j, i - j)
-#                 idx = idx + 1
-#                 if idx >= self.num_filters + self.start:
-#                     return basis_64.reshape((self.num_filters, 1, self.scale, self.scale))
-#
-#     def dct2d_Conv(self):
-#         return nn.Conv2d(in_channels=1, out_channels=self.num_filters, kernel_size=self.scale, stride=self.scale,
-#                           bias=False)
-#
-#     def forward(self, input):
-#         # bs, _,_,_ = input.shape()
-#         # input.view(bs*(128//self.scale)**2, 3, self.scale,self.scale)
-#         dct_outs = torch.cat([self.conv1(input[:, 0:1, ...]), self.conv2(input[:, 1:2, ...]), self.conv3(input[:, 2:3, ...])], dim=1)
-#         dct_reallocate = torch.cat([dct_outs[:, index:index + 1, ...] for index in self.swap], dim=1)
-#         return dct_reallocate
+
 
 class HighResolutionModule(nn.Module):
     def __init__(self, num_branches, blocks, num_blocks, num_inchannels,
@@ -365,11 +260,10 @@ blocks_dict = {
 }
 
 
-class SplicingDetectionNet(nn.Module):
-
+class CAT_Net(nn.Module):
     def __init__(self, config, **kwargs):
         extra = config.MODEL.EXTRA
-        super(SplicingDetectionNet, self).__init__()
+        super(CAT_Net, self).__init__()
 
         # RGB branch
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1,
@@ -428,7 +322,6 @@ class SplicingDetectionNet(nn.Module):
             nn.BatchNorm2d(64, momentum=BN_MOMENTUM),
             nn.ReLU(inplace=True)
         )
-        # self.dc_layer1 = self._make_layer(HalfDilatedBlock, inplanes=32, planes=32, blocks=1, stride=1)
         self.dc_layer1_tail = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=4, kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(4, momentum=BN_MOMENTUM),
@@ -458,7 +351,6 @@ class SplicingDetectionNet(nn.Module):
 
         DC_final_stage_channels.insert(0, 0)  # to match # branches
 
-
         # stage 5
         self.stage5_cfg = extra['STAGE5']
         num_channels = self.stage5_cfg['NUM_CHANNELS']
@@ -487,7 +379,6 @@ class SplicingDetectionNet(nn.Module):
                 stride=1,
                 padding=1 if extra.FINAL_CONV_KERNEL == 3 else 0)
         )
-
 
     def _make_transition_layer(
             self, num_channels_pre_layer, num_channels_cur_layer):
@@ -574,7 +465,7 @@ class SplicingDetectionNet(nn.Module):
     def forward(self, x, qtable):
         RGB, DCTcoef = x[:, :3, :, :], x[:, 3:, :, :]
 
-        # RGB branch
+        # RGB Stream
         x = self.conv1(RGB)
         x = self.bn1(x)
         x = self.relu(x)
@@ -607,9 +498,8 @@ class SplicingDetectionNet(nn.Module):
                 x_list.append(y_list[i])
         RGB_list = self.stage4(x_list)
 
-        # DC coefficient branch
+        # DCT Stream
         x = self.dc_layer0_dil(DCTcoef)
-        # x = self.dc_layer1(x)
         x = self.dc_layer1_tail(x)
         B, C, H, W = x.shape
         x0 = x.reshape(B, C, H // 8, 8, W // 8, 8).permute(0, 1, 3, 5, 2, 4).reshape(B, 64 * C, H // 8,
@@ -664,46 +554,36 @@ class SplicingDetectionNet(nn.Module):
         logger.info('=> init weights from normal distribution')
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                # if m.kernel_size==(8,8):
-                #     continue
                 nn.init.normal_(m.weight, std=0.001)
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
         if os.path.isfile(pretrained_rgb):
-            # loaded_dict = torch.load(pretrained_rgb)['state_dict']
             loaded_dict = torch.load(pretrained_rgb)
             model_dict = self.state_dict()
-            # loaded_dict = {k.replace('dc_', 'RGB_'):v for k, v in loaded_dict.items()}
             loaded_dict = {k: v for k, v in loaded_dict.items()
                                if k in model_dict.keys() and not k.startswith('lost_layer.')}  # RGB weight
             logger.info('=> (RGB) loading pretrained model {} ({})'.format(pretrained_rgb, len(loaded_dict)))
             model_dict.update(loaded_dict)
             self.load_state_dict(model_dict)
-            #for param in self.named_parameters():
-            #    if param[0] in loaded_dict.keys():
-            #        param[1].requires_grad = True  # freeze RGB part
         else:
             logger.warning('=> Cannot load pretrained RGB')
         if os.path.isfile(pretrained_dct):
             loaded_dict = torch.load(pretrained_dct)['state_dict']
             model_dict = self.state_dict()
             loaded_dict = {k: v for k, v in loaded_dict.items()
-                               if k in model_dict.keys()}  # DC weight
+                               if k in model_dict.keys()}
             loaded_dict = {k:v for k,v in loaded_dict.items()
                            if not k.startswith('last_layer')}
             logger.info('=> (DCT) loading pretrained model {} ({})'.format(pretrained_dct, len(loaded_dict)))
             model_dict.update(loaded_dict)
             self.load_state_dict(model_dict)
-            #for param in self.named_parameters():
-            #    if param[0] in loaded_dict.keys():
-            #       param[1].requires_grad = True  # False = freeze DCT part
         else:
             logger.warning('=> Cannot load pretrained DCT')
 
 
 def get_seg_model(cfg, **kwargs):
-    model = SplicingDetectionNet(cfg, **kwargs)
+    model = CAT_Net(cfg, **kwargs)
     model.init_weights(cfg.MODEL.PRETRAINED_RGB, cfg.MODEL.PRETRAINED_DCT)
 
     return model
